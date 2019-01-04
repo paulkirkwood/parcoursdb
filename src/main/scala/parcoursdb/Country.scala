ba3d1d35e68d3db5f98353725e8a32aa5b73aab8
Paul Kirkwood <paul@paulandsue.plus.com>
2019-01-04 19:36:16 +0000

    Add countries from TdF 2018 rider nationalities

--- src/main/scala/parcoursdb/Country.scala
+++ src/main/scala/parcoursdb/Country.scala
@@ -3,20 +3,26 @@ package parcoursdb
 sealed trait Country
 case object Andorra extends Country
 case object Austria extends Country
+case object Australia extends Country
 case object Belgium extends Country
 case object Canada extends Country
+case object Colombia extends Country
+case object Denmark extends Country
 case object France extends Country
 case object Germany extends Country
 case object WestGermany extends Country
 case object Greece extends Country
 case object Ireland extends Country
-case object Israel extends Country // 2018 Giro d'Italia start
+case object Israel extends Country
 case object Italy extends Country
-case object Luxembourg extends Country // 1989 Tour de France start
+case object Luxembourg extends Country
+case object Norway extends Country
 case object Monaco extends Country
 case object Netherlands extends Country
+case object Poland extends Country
 case object Spain extends Country
 case object SanMarino extends Country
 case object Slovenia extends Country
+case object Slovakia extends Country
 case object Switzerland extends Country
 case object UnitedKingdom extends Country
